a1e03b97d335243e3b671d558cccd0a2ff8ac994
jz <byjzww@163.com>
2022-03-04 11:34:23 +0800

    更新路由配置，设置测试页面

---
+++ config/routes.ts
@@ -0,0 +1,18 @@
+const isDev = process.env.NODE_ENV === 'development';
+
+var routes = [
+  // { exact: false, path: '*', redirect: '/login' },
+  { exact: true, path: '/', component: '@/pages/Index' },
+  { exact: true, path: '/login', component: '@/pages/Login' },
+  { exact: true, path: '/chat', component: '@/components/Chat' },
+  { component: '@/pages/404' },
+];
+
+if (isDev) {
+  routes = [
+    { exact: true, path: '/test', component: '@/pages/Test' },
+    ...routes,
+  ];
+}
+
+export default routes;
