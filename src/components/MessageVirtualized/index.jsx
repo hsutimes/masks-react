bfb4487eb88f6ca2c81ca630b06731b90c649cd8
jz <byjzww@163.com>
2022-03-11 11:34:50 +0800

    非等高元素无限滚动加载

--- src/components/MessageVirtualized/index.jsx
+++ src/components/MessageVirtualized/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Avatar } from 'antd';
+import { useLocalStorageState } from 'ahooks';
 import { scrollToY } from '@/utils/sliding-scroll';
 import { List, AutoSizer } from 'react-virtualized';
 import 'react-virtualized/styles.css';
@@ -8,9 +9,60 @@ import styles from './index.less';
 const MessageVirtualized = (props) => {
   const msg = props.msg;
 
+  const estimatedItemSize = 80; // 估计节点高度
+  const time = 500; // 淡入淡出 500ms
+  const [positions, setPositions] = useLocalStorageState('positions'); // 缓存节点真实高度
+
+  const [scrollToIndex, setScrollToIndex] = useState(undefined); // 滚动到目标索引
+
   useEffect(() => {
     if (msg?.length > 0) {
-      toBottom();
+      // 滚动到最底部
+      setScrollToIndex(msg.length - 1);
+      // 初始化节点高度
+      let t = msg.map((_, index) => ({
+        index,
+        height: estimatedItemSize,
+      }));
+      if (positions?.length > 0) {
+        if (msg.length == positions.length) {
+          t = [...positions];
+        } else if (msg.length > positions.length) {
+          // 新消息，淡入淡出
+          toBottom(time);
+          setScrollToIndex(undefined);
+          positions.map((v, i) => {
+            t[i].height = v.height;
+          });
+        } else {
+          // 如果消息数小于缓存数，清空缓存
+          setPositions(t);
+        }
+      }
+      // 获取节点真实高度
+      let ele = document.getElementById('scroll').childNodes[0];
+      let nodes = ele.childNodes;
+      nodes.forEach((node, k) => {
+        let rect = node.getBoundingClientRect();
+        let height = rect.height;
+        let index = parseInt(node.id);
+        let oldHeight = t[index].height;
+        let dValue = oldHeight - height;
+        // 存在差值
+        if (dValue) {
+          t[index].height = height;
+          // 更新节点高度与滚动面板高度
+          node.style.setProperty('min-height', height + 'px', 'important');
+          let h =
+            ele.getBoundingClientRect().height + height - estimatedItemSize;
+          ele.style.setProperty('height', h + 'px', 'important');
+          ele.style.setProperty('max-height', h + 'px', 'important');
+        }
+      });
+      // console.log(t);
+      // 更新缓存
+      setPositions(t);
+      // if (!scrollToIndex) toBottom(time);
     }
   }, [msg]);
 
@@ -19,27 +71,51 @@ const MessageVirtualized = (props) => {
     else return obj.user;
   };
 
-  const toBottom = () => {
+  const toBottom = (t) => {
     let ele = document.getElementById('scroll');
-    scrollToY(ele.scrollHeight, 500, ele);
+    scrollToY(ele.scrollHeight, t, ele);
   };
 
-  function rowRenderer({
+  const rowRenderer = ({
     key, // Unique key within array of rows
     index, // Index of row within collection
     isScrolling, // The List is currently being scrolled
     isVisible, // This row is visible within the List (eg it is not an overscanned row)
     style, // Style object to be applied to row (to position it)
-  }) {
+  }) => {
     let i = msg[index];
     return (
-      <li key={key} style={style} className={i.isMe ? styles.me : styles.other}>
+      <li
+        id={index}
+        key={key}
+        // style={style}
+        style={{
+          minHeight: style.height,
+          left: style.left,
+          position: style.position,
+          top: style.top,
+          width: style.width,
+        }}
+        className={i.isMe ? styles.me : styles.other}
+      >
         <Avatar style={i.avatar_color}>{user(i)}</Avatar>
         <div style={{ margin: '7px 56px 0px' }}>{i.user}</div>
         <p className={styles.message}>{i.data}</p>
       </li>
     );
-  }
+  };
+
+  const getRowHeight = ({ index }) => {
+    let h = estimatedItemSize;
+    // 取节点真实高度
+    if (positions?.length > 0) {
+      let i = positions[index];
+      if (i?.height) {
+        h = i.height;
+      }
+    }
+    return h;
+  };
 
   return (
     <>
@@ -52,9 +128,9 @@ const MessageVirtualized = (props) => {
                 height={height}
                 width={width}
                 rowCount={msg.length}
-                rowHeight={80}
+                rowHeight={getRowHeight}
                 rowRenderer={rowRenderer}
-                // scrollToIndex={msg.length - 1}
+                scrollToIndex={scrollToIndex}
               />
             )}
           </AutoSizer>
