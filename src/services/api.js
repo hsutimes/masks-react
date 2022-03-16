1956a1ed4d2081f700e93538c5b8dceb83b5ccb0
jz <byjzww@163.com>
2022-03-16 11:45:17 +0800

    更新api

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,7 @@
 
 /* eslint-disable */
 import { request } from 'umi';
+import { fetchProgress } from '@/utils/fetchProgress';
 
 /** 上传图片  */
 export function uploadImageList (body) {
@@ -24,4 +25,18 @@ export function uploadImageOne (body) {
     body: form,
     headers: {},
   });
+}
+
+/** 上传图片，显示进度条  */
+export function uploadImageProgress (body, onProgress) {
+  let form = new FormData();
+  form.append('key', '26a43fa810d6bbd4bbf2a8052acce63e');
+  form.append('action', 'upload');
+  form.append('format', 'json');
+  form.append('source', body.file);
+  return fetchProgress('http://demo.hsutimes.com:7005/api/1/upload', {
+    method: 'POST',
+    body: form,
+    headers: {},
+  }, onProgress);
 }
