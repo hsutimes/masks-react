73614a9c7c7dedac91be6a607b411b5f4ef01e55
jz <byjzww@163.com>
2022-12-09 15:16:17 +0800

    更新ImageViewer 图片查看器

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Avatar, Button, Image } from 'antd';
+import React, { useState, useEffect, useRef } from 'react';
+import { Avatar } from 'antd';
+import { Image, ImageViewer } from 'antd-mobile';
+
 import { scrollToY } from '@/utils/sliding-scroll';
 import { useUpdateEffect } from 'ahooks';
 import Markdown from 'marked-react';
@@ -26,7 +28,11 @@ const renderer = {
 const Message = (props) => {
   // const msg = props.msg;
 
+  const ref = useRef();
+
+  const [visible, setVisible] = useState(false);
   const [msg, setMsg] = useState([]);
+  const [imgList, setImgList] = useState([]);
 
   const [count, setCount] = useState(0);
 
@@ -37,6 +43,13 @@ const Message = (props) => {
       if (msgArr.length > 100) {
         msgArr = msgArr.splice(msgArr.length - 100, msgArr.length);
       }
+      let t = [];
+      msgArr.map((i) => {
+        if (i.data?.includes('img|')) {
+          t.push(i.data.split('|')[1]);
+        }
+      });
+      setImgList(t);
       setMsg(msgArr);
     }
   }, [props.msg]);
@@ -62,58 +75,78 @@ const Message = (props) => {
     scrollToY(ele.scrollHeight, t, ele);
   };
 
+  const onImgClick = (e) => {
+    let index = imgList.findIndex((i) => i == e);
+    console.log(index);
+    if (index != -1) {
+      ref.current.swipeTo(index);
+      setVisible(true);
+    }
+  };
+
   return (
     <>
       <div id="scroll" className={styles.chat}>
-        <Image.PreviewGroup>
-          {msg.map((i, k) => (
+        {msg.map((i, k) => (
+          <div
+            key={k}
+            className={classNames(
+              styles.item,
+              i.isMe ? styles.me : styles.other,
+            )}
+          >
             <div
-              key={k}
-              className={classNames(
-                styles.item,
-                i.isMe ? styles.me : styles.other,
-              )}
+              className={styles.avatar}
+              style={{ opacity: i.isOnlyMsg ? 0 : 1 }}
             >
-              <div
-                className={styles.avatar}
-                style={{ opacity: i.isOnlyMsg ? 0 : 1 }}
-              >
-                <Avatar style={i.avatar_color} size={38}>
-                  {user(i)}
-                </Avatar>
-              </div>
-              <div className={styles.msg_body}>
-                {!i.isOnlyMsg && conf.settings.isShowNickname && (
-                  <div className={styles.nickname}>
-                    <span>
-                      {conf.settings.isAnonymity ? 'Anonymity' : i.user}
-                    </span>
-                  </div>
-                )}
-                {i.data?.includes('img|') ? (
-                  <div className={styles.img}>
-                    <Image
-                      src={i.data.split('|')[1]}
-                      style={{ borderRadius: 8 }}
-                      alt=""
+              <Avatar style={i.avatar_color} size={38}>
+                {user(i)}
+              </Avatar>
+            </div>
+            <div className={styles.msg_body}>
+              {!i.isOnlyMsg && conf.settings.isShowNickname && (
+                <div className={styles.nickname}>
+                  <span>
+                    {conf.settings.isAnonymity ? 'Anonymity' : i.user}
+                  </span>
+                </div>
+              )}
+              {i.data?.includes('img|') ? (
+                <div
+                  className={styles.img}
+                  onClick={() => {
+                    onImgClick(i.data.split('|')[1]);
+                  }}
+                >
+                  <Image
+                    src={i.data.split('|')[1]}
+                    fit="cover"
+                    style={{ borderRadius: 8 }}
+                  />
+                </div>
+              ) : (
+                <div className={styles.msg_bg}>
+                  <div className={styles.message}>
+                    <Markdown
+                      value={i.data}
+                      renderer={renderer}
+                      breaks={true}
                     />
                   </div>
-                ) : (
-                  <div className={styles.msg_bg}>
-                    <div className={styles.message}>
-                      <Markdown
-                        value={i.data}
-                        renderer={renderer}
-                        breaks={true}
-                      />
-                    </div>
-                  </div>
-                )}
-              </div>
+                </div>
+              )}
             </div>
-          ))}
-        </Image.PreviewGroup>
+          </div>
+        ))}
       </div>
+      <ImageViewer.Multi
+        ref={ref}
+        images={imgList}
+        visible={visible}
+        onClose={() => {
+          setVisible(false);
+        }}
+      />
     </>
   );
 };
